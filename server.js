0815c1a86ea70b65d6f13c48b4448828fc2d0ef9
sandeep raj <sandeep.raj@techolution.com>
2018-09-24 16:43:17 +0530

    whitelist

--- server.js
+++ server.js
@@ -93,10 +93,11 @@ export default function(opt) {
         }
 
         const reqId = parts[1];
-
+        const allowedSubDomains = ['app', 'api', 'xyz', 'test', 'fr', 'client', 'client-test', 'admin', 'guest', 'proxy'];
         // limit requested hostnames to 63 characters
-        if (! /^(?:[a-z0-9][a-z0-9\-]{4,63}[a-z0-9]|[a-z0-9]{4,63})$/.test(reqId)) {
-            const msg = 'Invalid subdomain. Subdomains must be lowercase and between 4 and 63 alphanumeric characters.';
+        // if (! /^(?:[a-z0-9][a-z0-9\-]{4,63}[a-z0-9]|[a-z0-9]{4,63})$/.test(reqId)) {
+        if (allowedSubDomains.indexOf(reqId) === -1) {
+            const msg = 'Invalid subdomain. Subdomains must be one of :' + allowedSubDomains.join(', ');
             ctx.status = 403;
             ctx.body = {
                 message: msg,
